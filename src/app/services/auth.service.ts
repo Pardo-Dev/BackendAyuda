db9bb7326880508c370d43a5879581280638c635
Maximiliano <max.rodriguezu@duocuc.cl>
2024-11-05 23:07:18 -0300

    second_commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,50 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  url_api = "https://dummyjson.com/auth/login"
+
+  constructor(
+    // Inyeccion de servicio HTTP
+    private http: HttpClient
+  ) { }
+
+  // Metodo del login
+  login(username: string, password: string): Observable<User>{
+    const headers = new HttpHeaders({'Content-Type': 'application/json'})
+    const body = { username, password}
+
+    // Lo envia a la API y retorna la respuesta: TOKEN + JSON DATOS
+    return this.http.post<User>(this.url_api, body, { headers });
+  }
+
+  // Insertar token
+  insert_token(token: string ): void {
+    localStorage.setItem('accessToken', token)
+  }
+
+  // Obtener el token
+  obtener_token(): string | null{
+    return localStorage.getItem('accessToken')
+  }
+
+  // Metodo para obtener Headers y Token para otras solicitudes
+  obtener_headers(): HttpHeaders{
+    const token = this.obtener_token();
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+  }
+
+
+
+
+
 }
